20dc916cc79f20e4de9f3072fd8a24f1dbb5cd3e
Deni Irawan Nugraha <60280044+deni-irawan40563@users.noreply.github.com>
2020-09-15 23:35:40 +0700

    project new first commit

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# frontend-chatapp
+# ff
 
 ## Project setup
 ```
---
+++ src/views/Home.vue
@@ -0,0 +1,19 @@
+<template>
+<div>
+  <div class="container">
+      <formSubmit />
+  </div>
+</div>
+</template>
+
+<script>
+// @ is an alias to /src
+import formSubmit from '@/components/formSubmit.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    formSubmit
+  }
+}
+</script>
